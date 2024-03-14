09abf948fdcc60250fb1a1f2fd42ab98c77da6ac
Rohiedits <142705213+Rohiedits@users.noreply.github.com>
2024-03-14 11:40:58 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React, { useState } from "react";
+
+function App () {
+ useState();
+
+    return (
+        <div className="App">
+            <h1>Hello hooks with Rohi</h1>
+        </div>
+    );
+};
+       export default App;
